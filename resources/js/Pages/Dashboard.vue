622d60463797b0f4b4e1afea18a00ba7d6aa2ac2
brayan2903 <71749066+brayan2903@users.noreply.github.com>
2024-06-09 09:57:04 -0500

    web site

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,38 @@
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            Dashboard
+        </template>
+
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 border-b border-gray-200">
+                You're logged in!dddddd123
+            </div>
+        </div>
+        <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-card title="Default size card" style="width: 300px">
+    <template #extra><a href="#">more</a></template>
+    <p>card content</p>
+    <p>card content</p>
+    <p>card content</p>
+  </a-card>
+  <div>{{ modal }}
+    <a-button type="primary" @click="modal = true">Open Modal</a-button>
+    <a-modal v-model:open="modal" title="Basic Modal" @ok="handleOk">
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </a-modal>
+  </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import { Head } from '@inertiajs/vue3';
+import {ref} from 'vue';
+const modal=ref(false)
+</script>
